72f37ff886390c4137eb814592ed1eee662ee287
sentinel46 <altdaedroth@gmail.com>
2019-02-24 23:00:29 +0300

    log_analyzer's update by code review

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import glob
+import datetime
 import gzip
 import json
 import logging
 import optparse
 import os
 import re
-import statistics
 import sys
+
+from statistics import median
+from string import Template
+from tempfile import NamedTemporaryFile
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -29,10 +32,9 @@ def update_config(config_file_name, config_):
     """Update default config with data from file in json format"""
 
     with open(config_file_name) as config_file:
-        new_config = json.load(config_file)
+        config_from_file = json.load(config_file)
 
-    config_.update(new_config)
-    return config_
+    config_.update(config_from_file)
 
 
 def find_last_log_file(pattern, log_dir="."):
@@ -41,16 +43,17 @@ def find_last_log_file(pattern, log_dir="."):
     returns file_name and it's timestamp string
     """
 
-    log_files = {}
-    for name in glob.glob(os.path.join(log_dir, "*")):
-        match = re.match(pattern, name)
+    log_file_name = str()
+    last_date = datetime.date(datetime.MINYEAR, 1, 1)
+    for file_name in os.listdir(log_dir):
+        match = re.match(pattern, file_name)
         if match:
-            log_files.update({match.group("date"): name})
-    if not log_files:
-        return None
+            date = datetime.datetime.strptime(match.group("date"), "%Y%m%d").date()
+            if date > last_date:
+                last_date = date
+                log_file_name = file_name
 
-    last_date = sorted(log_files.keys(), reverse=True)[0]
-    return log_files[last_date], last_date
+    return None if not log_file_name else (log_file_name, last_date)
 
 
 def parse_log_file(pattern, file_name, error_rate):
@@ -89,9 +92,10 @@ def parse_log_file(pattern, file_name, error_rate):
                 succeed += 1
             else:
                 logging.info("Cannot parse line: {}".format(line))
+    print("{} log parsing finished".format(file_name))
 
     if succeed / total < 1 - error_rate:
-        return
+        return None
 
     info = {"total": total, "succeed": succeed, "total_time": total_time}
 
@@ -110,7 +114,7 @@ def get_stats(url, request_times, succeed, total_time):
             "time_perc": round(100 * time_sum / total_time, 3),
             "time_avg": round(time_sum / count, 3),
             "time_max": round(max(request_times), 3),
-            "time_med": round(statistics.median(request_times), 3)}
+            "time_med": round(median(request_times), 3)}
 
 
 def prepare_report(url_stats, info, report_size):
@@ -130,43 +134,27 @@ def write_report_to_html(report, report_template, report_file_name):
 
     json_information = json.dumps(report)
 
-    with open(report_template) as html_file:
-        html = html_file.read()
+    with open(report_template, mode="r", encoding="utf-8") as html_file:
+        html = Template(html_file.read())
 
-    html = html.replace("$table_json", json_information)
+    html = html.safe_substitute(table_json=json_information)
 
-    with open(report_file_name, "w") as report_file:
-        report_file.write(html)
+    with NamedTemporaryFile(mode="w", encoding="utf-8", dir=os.path.dirname(report_file_name)) as temp_report_file:
+        temp_report_file.write(html)
+        os.link(temp_report_file.name, report_file_name)
 
 
-def main():
-    parser = optparse.OptionParser()
-    parser.add_option("--config", dest="new_config", default=None, type="string")
-    options, args = parser.parse_args()
-
-    logger_config = config
-    if options.new_config:
-        try:
-            logger_config = update_config(options.new_config, logger_config)
-        except (IOError, json.JSONDecodeError, TypeError) as e:
-            sys.exit("Error while trying to read configuration file: {}".format(e))
-
-    logging.basicConfig(filename=logger_config["LOG_NAME"] if logger_config["LOG_NAME"] else None,
-                        format="[%(asctime)s] %(levelname).1s %(message)s",
-                        datefmt="%Y.%m.%d %H:%M:%S",
-                        level=logging.INFO)
-
-    try:
+def main(config_):
         nginx_file_name_pattern = re.compile(".*nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$")
-        result = find_last_log_file(nginx_file_name_pattern, logger_config["LOG_DIR"])
+        result = find_last_log_file(nginx_file_name_pattern, config_["LOG_DIR"])
         if not result:
             logging.info("Log file not found. Nothing to parse. Exit.")
             sys.exit()
-        log_file_name, timestamp = result
+        log_file_name, date = result
 
-        report_file_name = "report-{}.{}.{}.html".format(timestamp[:4], timestamp[4:6], timestamp[6:8])
-        report_file_name = os.path.join(logger_config["REPORT_DIR"], report_file_name)
-        if report_file_name in glob.glob(os.path.join(logger_config["REPORT_DIR"], "*")):
+        report_file_name = "report-{:04d}.{:02d}.{:02d}.html".format(date.year, date.month, date.day)
+        report_file_name = os.path.join(config_["REPORT_DIR"], report_file_name)
+        if os.path.isfile(report_file_name):
             logging.info("Log report already exists. Nothing to do. Exit.")
             sys.exit()
 
@@ -176,16 +164,35 @@ def main():
             "\"(?P<http_referer>.+)\"\s+\"(?P<http_user_agent>.+)\"\s+\"(?P<http_x_forwarded_for>.+)\"\s+" \
             "\"(?P<http_X_REQUEST_ID>.+)\"\s+\"(?P<http_X_RB_USER>.+)\"\s+(?P<request_time>.+)")
 
-        result = parse_log_file(nginx_log_pattern, log_file_name, logger_config["ERROR_RATE"])
+        result = parse_log_file(nginx_log_pattern, os.path.join(config_["LOG_DIR"], log_file_name), config_["ERROR_RATE"])
         if not result:
-            logging.error("Too many log file parsing errors. Abort.")
+            logging.error("More than {}% errors occurred during parsing log file. Abort."
+                          .format(config_["ERROR_RATE"] * 100))
             sys.exit()
         url_stats, info = result
-        report = prepare_report(url_stats, info, logger_config["REPORT_SIZE"])
+        report = prepare_report(url_stats, info, config_["REPORT_SIZE"])
         write_report_to_html(report, "./report.html", report_file_name)
-    except Exception as e:
-        logging.exception("Unhandled error:\n{}".format(e))
+        print("All operations completed. Report saved into {}".format(report_file_name))
 
 
 if __name__ == "__main__":
-    main()
+    parser = optparse.OptionParser()
+    parser.add_option("--config", dest="new_config", default=None, type="string")
+    options, args = parser.parse_args()
+
+    logger_config = config.copy()
+    if options.new_config:
+        try:
+            update_config(options.new_config, logger_config)
+        except (IOError, json.JSONDecodeError, TypeError) as e:
+            sys.exit("Error while trying to read configuration file: {}".format(e))
+
+    logging.basicConfig(filename=logger_config["LOG_NAME"] if logger_config["LOG_NAME"] else None,
+                        format="[%(asctime)s] %(levelname).1s %(message)s",
+                        datefmt="%Y.%m.%d %H:%M:%S",
+                        level=logging.INFO)
+
+    try:
+        main(logger_config)
+    except Exception as e:
+        logging.exception("Unhandled error:\n{}".format(e))
